aaa0b91dcffdedd197ba667dcfcc01717805bce5
liloow <tom.beguin@gmail.com>
2018-07-09 07:13:40 +0200

    added route for sensitibility

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,17 @@ const router = express.Router(); // eslint-disable-line
 const watcher = require('../index');
 
 router.post('/', function(req, res, next) {
-  console.log(req.body);
   const data = req.body;
   let result = watcher.processStream(data);
   return res.json(result);
 });
+
+router.post('/', function(req, res, next) {
+  const {treshold, cycle, limit} = req.body;
+  const pattern = setSensibility(treshold, cycle, limit);
+  return res.json(pattern);
+});
+
 router.get('/', function(req, res, next) {
   const status = watcher.currentStatus();
   res.json(status);
